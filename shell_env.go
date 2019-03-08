ceb233d3315289e9b0cff809104f8819fadfdc54
rodgef <rodgef101@gmail.com>
2019-03-08 14:20:11 +0200

    initialised shell state

--- shell_env.go
+++ shell_env.go
@@ -1,18 +1,26 @@
 package main
 
-//import (
-//	"os"
-//)
+import (
+	"os"
+)
 
-type state struct{
+type shell_state struct{
 	input			string  
 	env 			[]string 
 	oldpwd 			string 	
-	currpwd 		string 
+	pwd 		string 
 }
 
-func initState(input string) state {
-	ret := state{}
+func initState(input string) shell_state {
+	pwd, err := os.Getwd()
+	ret := shell_state{}
+	if err == nil{
+		env  := os.Environ()
+		ret.input = input
+		ret.env  = env
+		ret.pwd = pwd
+		ret.oldpwd = ret.pwd
+	}
 	return ret
 
 }
